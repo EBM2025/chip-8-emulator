ef5194ffd0e33938942306b08b05ef94b9751980
Ellis Mandel <ellismandel@Mac.router914d02.com>
2025-12-14 23:03:24 -0500

    init code changes

---
+++ chip8/src/lib.rs
@@ -0,0 +1,73 @@
+const RAM_SIZE: usize = 4096; // chip8 implemented for 4KB RAM
+const NUM_REG: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+const START_ADDR: u16 = 0x200;
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+];
+
+pub const WIDTH: usize = 64;
+pub const HEIGHT: usize = 32;
+
+pub struct Emu {
+    pc: u16, // index of current instruction
+    ram [u8; RAM_SIZE],
+    screen: [bool; WIDTH * HEIGHT], // black and white pixel screen array
+    v_reg: [u8; NUM_REG],
+    i_reg: u16,
+    stck_ptr: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    delay_timer: u8, // count down every CPU cycle and performs some action
+    sound_timer: u8 // emits noise every CPU cycle
+}
+
+impl Emu {
+    pub fn new() -> Self {
+        let mut new_emu = Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        };
+
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONSET);
+    }
+
+    pub fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val; // push to top of the stack
+        self.sp += 1; // update the stack pointer
+    }
+
+    pub fn pop(&mut self) -> u16 {
+        self.sp -= 1; // sp points to open slot
+        self.stack[self.sp as usize];
+    }
+}
+
+
+
