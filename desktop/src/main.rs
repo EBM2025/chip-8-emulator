1ebfec7fab9f6e8b8f82c3ea976277930805cb69
Ellis Mandel <ellismandel@Mac.router914d02.com>
2025-12-16 21:54:44 -0500

    almost done

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -3,6 +3,12 @@ use std::fs::File;
 use std::io::Read;
 use chip8::*;
 use sdl2::*;
+use sdl2::event::Event;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::keyboard::Keycode;
 
 fn main() {
     let args: Vec<_> = env::args().collect(); // collect CL arguments from the user - args[0] stores the name of the program
@@ -13,7 +19,7 @@ fn main() {
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
     let window = video
-        .window("Chip-8 Emulator", WIDTH, HEIGHT)
+        .window("Chip-8 Emulator", W_WIDTH, W_HEIGHT)
         .position_centered()
         .opengl()
         .build()
@@ -35,12 +41,27 @@ fn main() {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit{..} => {
-                    break 'gameloop';
+                    break 'gameloop;
+                },
+                Event::KeyDown{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, true);
+                    }
+                },
+                Event::KeyUp{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, false);
+                    }
                 },
                 _ => ()
             }
         }
-        chip8.tick(); // CPU tick for each game loop
+        
+        for frame in 0..10 {
+            chip8.tick(); // CPU tick for each game loop
+        }
+        chip8.tick_timers(); 
+        draw_to_screen(&chip8, &mut canvas);
     }
 }
 
@@ -48,18 +69,41 @@ fn draw_to_screen(emulator: &Emulator, canvas: &mut Canvas<Window>) {
     // reset the canvas
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
-    canvas.set_draw_color(Color::RBG(255, 255, 255)); // set canvas to white and locate black pixels
+    canvas.set_draw_color(Color::RGB(255, 255, 255)); // set canvas to white and locate black pixels
     
     for (i, pixel) in emulator.get_display().iter().enumerate() {
-        if (pixel == 0) {
+        if *pixel {
             // convert from 1d array to 2d 
-            let x = (i % WIDTH) as u32;
-            let y = (i / WIDTH) as u32;
+            let x = (i % (WIDTH as usize)) as u32;
+            let y = (i / (WIDTH as usize)) as u32;
 
             // draw the pixel rect
-            let rect = Rect::new(x as i32, y as i32, WIDTH, HEIGHT);
+            let rect = Rect::new((x * SCALER) as i32, (y * SCALER) as i32, SCALER, SCALER);
             canvas.fill_rect(rect).unwrap();
         }
     }
     canvas.present();
+}
+
+fn key2btn(key: Keycode) -> Option<usize> {
+    // convert keypress to u8 for backend processing
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
 }
